a84f2722fceff7f0587312b9d66475fc1d4bc64d
Lucas Infante <maccinza@gmail.com>
2015-12-10 17:06:04 -0200

    Tries to fix path to import variable from package and run on deploy.

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ __author__ = 'infante'
 
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 from flask.ext.script import Manager, Server
 from subsfixer import app
 
