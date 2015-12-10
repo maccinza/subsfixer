05d8c59632b4e1eaa517c31e65c23ea17e4fcadf
Lucas Infante <maccinza@gmail.com>
2015-12-10 15:45:54 -0200

    Updates configuration for deploy.

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import os
 from flask import Flask
-
 
 # App configuration
 
 app = Flask(__name__)
-app.debug = True
+app.debug = False
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
