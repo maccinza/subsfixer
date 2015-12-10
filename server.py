968044db5a2f6d1616d80310bfac170089f8fe0e
Lucas Infante <maccinza@gmail.com>
2015-12-10 15:56:33 -0200

    Updates server file for deploy.

--- server.py
+++ server.py
@@ -8,5 +8,8 @@ from flask import Flask
 app = Flask(__name__)
 app.debug = False
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    from views import *
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
