3137cd656f5fc783115e9cda4a47ac6602c4e7aa
Lucas Infante <maccinza@gmail.com>
2015-12-10 18:38:07 -0200

    Still tries to fix app to run correctly on heroku deploy.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 u"""
 
 """
+import os
 from flask import Flask
 
 __author__ = 'infante'
@@ -10,5 +11,5 @@ app = Flask(__name__)
 app.debug = False
 
 if __name__ == "__main__":
-    app.run()
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
